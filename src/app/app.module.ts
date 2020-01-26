96515a6f405e368b577a09b640d3cf6c4b94204d
gumaceddo <gustavo-oliveira-macedo@hotmail.com>
2020-01-26 17:31:48 -0300

    adicao do modulo *MATERIAL*| retirada de um link inutilizavel do index.html |

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
-
-
+/*---------------- MDBootstrap ----------------------------- */
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+/* ------------------------------------------------------- */
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -18,16 +18,14 @@ import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ConsultaUsuarioService } from './service/consulta-usuario.service';
 import { HomeComponent } from './home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
 import { FaqComponent } from './faq/faq.component';
 import { AdminComponent } from './admin/admin.component';
 import { ComunidadesComponent } from './comunidades/comunidades.component';
 import { InserirProdutoComponent } from './inserir-produto/inserir-produto.component';
 import { AlterarProdutoComponent } from './alterar-produto/alterar-produto.component';
-
-
+/* Modulo do Material Angular que esta em pasta separada, afim de deixar o codigo mais Clean */
+import { MaterialModule } from './material/material.module';
+/* ----------------------------------------------------------------------------------------- */
 @NgModule({
   declarations: [
     AppComponent,
@@ -51,10 +49,10 @@ import { AlterarProdutoComponent } from './alterar-produto/alterar-produto.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule,
-    MatButtonModule
+    MDBBootstrapModule.forRoot(),
+    MaterialModule
+    
   ],
   providers: [ConsultaUsuarioService],
   bootstrap: [AppComponent]
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { FormsModule,ReactiveFormsModule } from '@angular/forms'
+import { CheckboxModule, WavesModule, ButtonsModule, InputsModule, IconsModule, CardsModule } from 'angular-bootstrap-md'
+
+
+
+const MaterialComponents = [
+  MatButtonModule,
+  BrowserAnimationsModule,
+  FormsModule,
+  CheckboxModule,
+  WavesModule,
+  ButtonsModule,
+  InputsModule,
+  IconsModule,
+  CardsModule,
+  ReactiveFormsModule
+]
+
+@NgModule({
+
+  imports: [MaterialComponents],
+  exports:[MaterialComponents]
+})
+export class MaterialModule { }
