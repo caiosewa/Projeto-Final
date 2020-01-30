cc8c230767116dd26cdcc380728ecaf1183cbe75
gumaceddo <gustavo-oliveira-macedo@hotmail.com>
2020-01-30 17:48:59 -0300

    feito alteração nos inputs

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,7 +4,11 @@ import {MatButtonModule} from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { FormsModule,ReactiveFormsModule } from '@angular/forms'
 import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
 import { CheckboxModule, WavesModule, ButtonsModule, InputsModule, IconsModule, CardsModule } from 'angular-bootstrap-md'
+import { from } from 'rxjs';
 
 const MaterialComponents = [
   MatButtonModule,
@@ -17,7 +21,12 @@ const MaterialComponents = [
   IconsModule,
   CardsModule,
   ReactiveFormsModule,
-  MatTableModule
+  MatTableModule,
+  MatFormFieldModule,
+  MatInputModule,
+
+  
+  
 ]
 
 @NgModule({
